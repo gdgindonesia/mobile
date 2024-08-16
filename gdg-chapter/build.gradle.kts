0dca1aa3ff850f8f2be4a7f4eae27855315d35a4
isfaaghyth <isfaaghyth@gmail.com>
2024-08-17 01:41:35 +0700

    feat: rearranged sourcesets and setup network and chapter modules

---
+++ gdg-chapter/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinxSerialization)
+}
+
+kotlin {
+    jvm()
+    androidTarget()
+    jvmToolchain(17)
+
+    val xcf = XCFramework()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "gdg-chapter"
+            xcf.add(this)
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":gdg-network"))
+        }
+    }
+}
+
+android {
+    namespace = "id.gdg.chapter"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+    }
+}
---
+++ gdg-chapter/src/commonMain/kotlin/id/gdg/chapter/model/ChapterModel.kt
@@ -0,0 +1,6 @@
+package id.gdg.chapter.model
+
+data class ChapterModel(
+    val id: Int,
+    val name: String
+)
