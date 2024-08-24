b5ef9a4159a5e622b2e38fd87c9fcbbe498b4e28
isfaaghyth <isfhani.ghiath@bytedance.com>
2024-08-25 04:14:44 +0700

    feat: add kover and setup github workflows

--- gdg-chapter/build.gradle.kts
+++ gdg-chapter/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kover)
 }
 
 kotlin {
